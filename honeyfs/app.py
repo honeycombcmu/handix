969c5f092590d3bce322847ba310f6e3cfd97474
handiXu <xuhandi2013@gmail.com>
2016-02-09 20:36:02 -0500

    add endpoints

--- honeyfs/app.py
+++ honeyfs/app.py
@@ -14,5 +14,15 @@ def health():
 def putfile():
     return 'ok'
 
+@app.route('/getfromfs/<path>', methods=['GET'])
+def getfile(path):
+    print path
+    return 'ok'
+
+@app.route('/mov/from/<path1>/<path2>', methods=['GET'])
+def movefile(path1, path2):
+    print path1, path2
+    return 'ok'
+
 if __name__ == '__main__':
     app.run()
